99117831d3c7b98eb7a0dbe88d091869591afec8
benperry2021 <50835226+benperry2021@users.noreply.github.com>
2020-01-26 13:06:26 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -5,7 +5,40 @@ public class Lab2
     Robot.load("square.txt");
     Robot.setDelay(0.1);
     
-    //INSERT CODE HERE
+    //Robot is in front of dark squares
+    cleanRow();
+    cleanRow();
+    cleanRow();
+    cleanRow();
+  }
+  
+  public static void cleanSpot()
+  {
+      //Robot is in front of a dark square
+      Robot.move();
+      Robot.makeLight();
+      //Robot is on a lightened square
+  }
+  
+  public static void cleanRow()
+  {
+      //Robot is facing towards dark squares
+      cleanSpot();
+      cleanSpot();
+      cleanSpot();
+      cleanSpot();
+      cleanSpot();
+      Robot.move();
+      Robot.turnLeft(); 
+      //Robot is ready for next row
+    
+  }
+  
+  public static void turnRight()
+  {
+    Robot.turnLeft();
+    Robot.turnLeft();
+    Robot.turnLeft();
   }
   
   public static void darkenComb()
@@ -14,6 +47,58 @@ public class Lab2
     Robot.setDelay(0.05);
     
     //INSERT CODE HERE
+    //Robot is looking up from the base of the comb
+    rowReady();
+    rowReady();
+    rowReady();
+    rowReady();
+    darkenRow();
+    //Robot has darkened all spots
+  }
+  
+  public static void darkenSpot()
+  {
+    Robot.move();
+    Robot.makeDark();
+  }
+  
+  public static void darkenRow()
+  {
+    //Robot is at start of a new row
+    turnRight();
+    Robot.makeDark();
+    darkenSpot();
+    darkenSpot();
+    darkenSpot();
+    darkenSpot();
+    darkenSpot();
+    darkenSpot();
+    Robot.turnLeft();
+    Robot.turnLeft();
+    Robot.move();
+    Robot.move();
+    Robot.move();
+    Robot.move();
+    Robot.move();
+    Robot.move();
+    turnRight();
+    //Robot is at the start of the row, looking up
+  }
+  
+  public static void rowReady()
+  {
+    //Robot is at the start of a row
+    darkenRow();
+    nextRow();
+  }
+  
+  public static void nextRow()
+  {
+    //Robot is ready for a new row
+    Robot.move();
+    Robot.makeDark();
+    Robot.move();
+    //Robot is at the next row
   }
   
   public static void makeCheckered()
@@ -22,5 +107,46 @@ public class Lab2
     Robot.setDelay(0.05);
     
     //INSERT CODE HERE
+    //Robot has blank checkerboard
+    twoRows();
+    newRowSet();
+    twoRows();
+    newRowSet();
+    twoRows();
+    newRowSet();
+    twoRows();
+  }
+  
+  public static void checkerRow()
+  {
+    Robot.makeDark();
+    Robot.move();
+    Robot.move();
+    Robot.makeDark();
+    Robot.move();
+    Robot.move();
+    Robot.makeDark();
+    Robot.move();
+    Robot.move();
+    Robot.makeDark();
+    Robot.move();
+    
+  }
+  
+  public static void twoRows()
+  {
+    checkerRow();
+    turnRight();
+    Robot.move();
+    turnRight();
+    checkerRow();
+  }
+  
+  public static void newRowSet()
+  {
+    Robot.turnLeft(); 
+    Robot.move();
+    Robot.turnLeft();
+    
   }
 }
